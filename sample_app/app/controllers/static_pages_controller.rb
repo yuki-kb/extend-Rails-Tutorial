3c57096e230682b3bfbde74d197ece3e3325b8ce
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-22 18:21:02 +0900

    Create the form for searching microposts

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -3,7 +3,16 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      if params[:search]
+        @feed_items = Micropost.paginate(page: params[:page])
+                      .where('content LIKE?',"%#{params[:search]}%")
+        if @feed_items.count == 0
+          flash[:info] = "Not find."
+          @feed_items = current_user.feed.paginate(page: params[:page])
+        end
+      else
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
     end
   end
 
--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -27,6 +27,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    # homeに移動して検索formが存在するか確認
+    get root_path
+    assert_template '/'
+    assert_select 'form'
+
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
